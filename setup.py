a4660e6299afbedae42445489d61a9c98ea2377d
Yegor Bitensky <yegorbitensky@gmail.com>
2019-05-03 00:21:52 +0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,46 @@
+# Copyright 2019 Yegor Bitensky
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='AGStuff',
+    version='1.0.0',
+    description="Abstract games stuff",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/YegorDB/AGStuff',
+    author='Yegor Bitensky',
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Topic :: Games/Entertainment",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+    keywords='cards',
+    packages=find_packages(exclude=['tests*', 'examples*']),
+    python_requires='>=3.6',
+)
